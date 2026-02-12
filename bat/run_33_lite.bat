f95b2b8a46baddca37fea794962accb2c156712f
RoadLabo <info@roadlabo.com>
2026-02-12 16:39:29 +0900

    Update run_33_lite.bat

--- bat/run_33_lite.bat
+++ bat/run_33_lite.bat
@@ -1,9 +1,23 @@
 @echo off
-cd /d %~dp0
+setlocal
+cd /d %~dp0\..
 
-set PY=%CD%\runtime\python\python.exe
-set SRC=%CD%\src\33_branch_check.py
+set "PY=%CD%\runtime\python\python.exe"
+set "SRC=%CD%\src\33_branch_check.py"
+
+if not exist "%PY%" (
+  echo [ERROR] python not found: "%PY%"
+  pause
+  exit /b 1
+)
+
+if not exist "%SRC%" (
+  echo [ERROR] script not found: "%SRC%"
+  pause
+  exit /b 1
+)
 
 "%PY%" "%SRC%" --nogui
 
 pause
+endlocal
