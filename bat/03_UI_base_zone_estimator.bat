06d85270549a4689078ada8c4eaa67d82d289f3b
RoadLabo <info@roadlabo.com>
2026-04-05 01:43:56 +0900

    Update 03_UI_base_zone_estimator.bat

--- bat/03_UI_base_zone_estimator.bat
+++ bat/03_UI_base_zone_estimator.bat
@@ -1,24 +1,12 @@
 @echo off
 setlocal EnableExtensions
 
-rem --- resolve repo root (this bat is in bat\) ---
 set "BAT_DIR=%~dp0"
 for %%I in ("%BAT_DIR%..") do set "ROOT_DIR=%%~fI"
 
 set "PYW=%ROOT_DIR%\runtime\python\pythonw.exe"
-set "PY=%ROOT_DIR%\runtime\python\python.exe"
-
 set "APP=%ROOT_DIR%\src\03_UI_base_zone_estimator.py"
 
-set "LOGDIR=%ROOT_DIR%\logs"
-if not exist "%LOGDIR%" mkdir "%LOGDIR%"
-set "LOG=%LOGDIR%\03_UI_base_zone_estimator_console.log"
-
-rem --- launch (NO start/cmd; direct pythonw/python) ---
-if exist "%PYW%" (
-  "%PYW%" "%APP%" 1>>"%LOG%" 2>>&1
-) else (
-  "%PY%" "%APP%" 1>>"%LOG%" 2>>&1
-)
-
-exit /b %errorlevel%
+cd /d "%ROOT_DIR%"
+start "" "%PYW%" "%APP%"
+exit /b 0
